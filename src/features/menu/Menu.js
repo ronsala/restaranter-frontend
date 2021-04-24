75e997414a0aa5f91676488f253f740e4297cc5c
RonSala <ron.sala@gmail.com>
2021-04-24 19:37:23 -0400

    develop menu

---
+++ src/features/menu/Menu.js
@@ -0,0 +1,10 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addMenu } from './menuSlice';
+// import styles from './Counter.module.css';
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
