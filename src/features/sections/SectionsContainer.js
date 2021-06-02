5959fe4d4d330a4e818858a2e3fedc9df40f4f7e
RonSala <ron.sala@gmail.com>
2021-06-02 16:41:42 -0400

    re-add conditional to useEffect in RestaurantContainer, use === in SectionsContainer

--- src/features/sections/SectionsContainer.js
+++ src/features/sections/SectionsContainer.js
@@ -4,23 +4,23 @@ import { fetchSections } from './sectionsSlice';
 import SectionCard from "./SectionCard";
 
 export const SectionsContainer = (props) => {
-  console.log('props in SectionsContainer:', props);
   const dispatch = useDispatch();
   const { status, error } = useSelector(state => state.sections);
+  const menuId = parseInt(props.menu?.id)
 
   useEffect(() => {
     if (props && props.menu) {
-      dispatch(fetchSections({restaurantId: props.menu?.attributes.restaurant_id, menuId: props.menu?.id}))
+      dispatch(fetchSections({restaurantId: props.menu.attributes.restaurant_id, menuId: menuId}))
     }
-  }, [dispatch, props])
+  }, [dispatch, props, menuId])
 
   const sections = Object
   .entries(useSelector((state) => state.sections.entities))
   .flat()
   .filter(element => typeof element === 'object')
-  .filter(section => section?.attributes.menu_id == props.menu?.id);
+  .filter(section => section.attributes.menu_id === menuId);
 
-  const sectionsList = sections?.map((section) => {
+  const sectionsList = sections.map((section) => {
     return <SectionCard key={section.id} name={section.attributes.name} id={section.id} />
   })
 
