dafa35486c4e37f0f1b9a683513bdc6ddca48fb3
RonSala <ron.sala@gmail.com>
2021-06-05 15:55:09 -0400

    tweek QuantityBox design

--- src/features/items/QuantityBox.js
+++ src/features/items/QuantityBox.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
-import Icon from '@material-ui/core/Icon';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    whiteSpace: 'nowrap'
+  },
   icon: {
-    fontSize: theme.typography.pxToRem(50),
+    fontSize: theme.typography.pxToRem(30),
   },
   quantity: {
     margin: theme.spacing(1),
@@ -22,16 +26,17 @@ const QuantityBox = () => {
     if (count >= 1) {
       setCount(count - 1)
     }
-    
   }
   
   return (
-    <div>
-      <Icon className={classes.icon} onClick={handleDecrementClick} color="primary">remove_circle</Icon>
-      <Typography variant="srOnly">Remove one</Typography>
+    <div className={classes.root}>
+      <RemoveCircleIcon className={classes.icon} onClick={handleDecrementClick} color="primary">
+        <Typography variant="srOnly">Remove one</Typography>
+      </RemoveCircleIcon>
       <TextField className={classes.quantity} id="outlined-basic" variant="outlined" name="quantity_box" value={count}/>
-      <Icon className={classes.icon} onClick={() => setCount(count + 1)} color="primary">add_circle</Icon>
-      <Typography variant="srOnly">Add one</Typography>
+      <AddCircleIcon className={classes.icon} onClick={() => setCount(count + 1)} color="primary">
+        <Typography variant="srOnly">Add one</Typography>
+      </AddCircleIcon>
     </div>
   )
 }
