f560c34326b0b82d8e23b873b0db2e8c7ee5d21f
RonSala <ron.sala@gmail.com>
2021-06-07 14:17:21 -0400

    rm unneeded code

--- src/features/orderitems/OrderItemsContainer.js
+++ src/features/orderitems/OrderItemsContainer.js
@@ -4,7 +4,6 @@ import OrderItemsTable from "./OrderItemsTable";
 
 export const OrderItemsContainer = () => {
   const orderitems = useSelector((state) => state.orderitems)
-  .filter(orderitem => orderitem.count > 0);
 
       return (
           <OrderItemsTable orderitems={orderitems} />
--- src/features/orderitems/orderitemsSlice.js
+++ src/features/orderitems/orderitemsSlice.js
@@ -16,7 +16,6 @@ export const orderitemsSlice = createSlice({
         state.push(payload)
       }
     },
-
   },
 })
 
