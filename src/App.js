0ce15deb33c3372c20e52ba24c5141a14a3f4389
RonSala <ron.sala@gmail.com>
2021-06-09 19:22:29 -0400

    adjust main div to width of AppBarMain

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
@@ -11,22 +12,33 @@ import Home from './components/Home'
 import AppBarMain from './components/AppBarMain';
 import { RestaurantsContainer } from './features/restaurants/RestaurantsContainer';
 import { RestaurantContainer } from './features/restaurants/RestaurantContainer';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    flexGrow: 1,
+    margin: '1rem',
+    position: 'relative',
+    bottom: 0,
+  },
+}));
 
 export const App = () => {
+  const classes = useStyles();
+
   return (
     <Router>
         <Container maxWidth="xl"> <Box color="secondary.main" display="flex">
             <Grid container>
               <Grid item xs={12}>
                 <AppBarMain />
-                <div className="main">
-                <Switch>
-                  <Route path="/restaurants/:restaurantId" component={RestaurantContainer} />
-                  <Route path="/restaurants" component={RestaurantsContainer} />
-                  <Route exact path="/" component={Home} />
-                 </Switch>
-                </div>
+                  <div className={classes.main}>
+                    <Switch>
+                      <Route path="/restaurants/:restaurantId" component={RestaurantContainer} />
+                      <Route path="/restaurants" component={RestaurantsContainer} />
+                      <Route exact path="/" component={Home} />
+                    </Switch>
+                  </div>
                  <Footer />
               </Grid>
             </Grid>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,12 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
     margin: '1rem',
-    position: 'relative',
+    position: 'fixed',
     bottom: 0,
+    left: 0,
+    right: 0,
+    flexGrow: 1,
   },
   copyright: {
     textAlign: 'center',
